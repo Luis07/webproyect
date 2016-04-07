e1c995e1eeee958ba44ce898296a10ce81ba5eb0
Luis07 <luisbrenes250594@gmail.com>
2016-04-07 14:39:52 -0600

    migracion
    migracion de la tabla correos

---
+++ proyectoweb1/database/migrations/2016_04_04_173427_create_emails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('destino');
+            $table->string('asunto');
+            $table->string('contenido');
+            $table->string('enviado');
+            $table->string('borrador');
+            $table->string('salida');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('mails');
+    }
+}
